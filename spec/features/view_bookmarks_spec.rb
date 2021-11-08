8b8ba4daebc8a3eb548ba33013dc389a158a5907
Mark McLeod <markmcleod45@gmail.com>
2021-11-08 15:48:38 +0000

    Add failing feature test for 'view all bookmarks. Driver/Navigator switch

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,6 +1,15 @@
 feature 'View bookmarks' do
+  
   scenario 'visit index page' do
     visit '/'
     expect(page).to have_content "Welcome to Bookmark Manager"
   end
+
+  scenario 'view all bookmarks' do
+    visit '/bookmarks'
+    expect(page).to have_content "BBC"
+    expect(page).to have_content "Google"
+    expect(page).to have_content "Youtube"
+  end
+
 end
